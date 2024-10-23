e0654f65a0138b9819917944344f3aafc5fef3f1
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-10-23 15:13:10 -0400

    Add typescript-eslint and drop eslint to v8
    The v9 flat config is not fun to use.
    Expo's eslint config still does not support it which causes clashes with
    the typescript-eslint config when using the v9 flat config.
    
    So, I dropped back to eslint v8.
    
    Also, expo's eslint config uses an old version of typescript-eslint
    so I had to drop my dependencies to that version

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+/* eslint-env node */
+
+/**
+ * @see https://eslint.org/docs/v8.x/use/configure/configuration-files
+ * @type {import("eslint").Linter.Config}
+ * */
+module.exports = {
+  root: true,
+  extends: [
+    'expo',
+    'plugin:tailwindcss/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+  ],
+  plugins: ['@typescript-eslint', 'tailwindcss'],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    tailwindcss: {
+      classRegex: '^[a-zA-Z]*(c|C)lassName$',
+      callees: ['classnames', 'clsx', 'ctl', 'cva', 'cx', 'twMerge'],
+    },
+  },
+  rules: {
+    'tailwindcss/no-arbitrary-value': 'warn',
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
+  },
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  overrides: [
+    {
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+      files: ['*.js', '*.cjs', '*.mjs'],
+    },
+  ],
+  ignorePatterns: ['/.expo', 'node_modules', 'android', 'ios'],
+};
--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,14 @@
-/* global __dirname -- node globals */
+/* eslint-env node */
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
 const nativewindConfig = withNativeWind(defaultConfig, {
-  input: "./src/app/global.css",
-  configPath: "./tailwind.config.ts",
+  input: './src/app/global.css',
+  configPath: './tailwind.config.ts',
 });
 
 module.exports = nativewindConfig;
