c7b675aefce66d62818d6680059c40f6e1ebc78d
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-10-23 11:46:36 -0400

    Add prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSameLine: true,
+  htmlWhitespaceSensitivity: "ignore",
+  trailingComma: "es5",
+};
