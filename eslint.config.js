61478a53d30d37dfa4bffbeaffa2a6c8ac947c21
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-10-23 13:46:46 -0400

    Add tailwind eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 const { FlatCompat } = require("@eslint/eslintrc");
 const js = require("@eslint/js");
+const tailwind = require("eslint-plugin-tailwindcss");
 
 // Compatibility layer for legacy eslint configurations
 // expo eslint config is still using the legacy format
@@ -8,4 +9,20 @@ const flatCompat = new FlatCompat();
 /**
  * @type {import('eslint').Linter.Config}
  */
-module.exports = [js.configs.recommended, ...flatCompat.extends("expo")];
+module.exports = [
+  js.configs.recommended,
+  ...flatCompat.extends("expo"),
+  ...tailwind.configs["flat/recommended"],
+  {
+    name: "tailwind",
+    rules: {
+      "tailwindcss/no-arbitrary-value": "warn",
+    },
+    settings: {
+      tailwindcss: {
+        classRegex: "^[a-zA-Z]*(c|C)lassName$",
+        callees: ["classnames", "clsx", "ctl", "cva", "cx", "twMerge"],
+      },
+    },
+  },
+];
