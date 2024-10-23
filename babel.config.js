0d270406dafc8876e86371450a24a4c3a529ae07
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-10-23 11:49:12 -0400

    Update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
-module.exports = function (api) {
-  api.cache(true);
+/**
+ * @see https://babeljs.io/docs/en/config-files#config-function-api
+ * @type {import("@babel/core").ConfigFunction}
+ * */
+module.exports = (api) => {
+  api.cache.forever();
+
   return {
-    presets: ['babel-preset-expo'],
+    presets: [["babel-preset-expo"]],
   };
 };
